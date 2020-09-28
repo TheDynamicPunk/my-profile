cbf0c8981929dce1bdafe4bc54996b1ea09ff2ba
Ashu <ashutoshgu18@gmail.com>
2020-09-29 01:20:46 +0530

    Remove commented code

--- load.js
+++ load.js
@@ -1,9 +1,3 @@
-// document.onreadystatechange = () => {
-//     if(document.readyState === "complete") {
-//         document.querySelector('.loader_pane').classList.add('fadeIn');
-//     }
-// }
-
 window.onload = () =>  {
     document.querySelector('.loader_pane').classList.add('fadeIn')
 }
